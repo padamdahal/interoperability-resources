a6618d1383f2e0aac705e1b64bd649174393cfb1
padamdahal <dahal0514@gmail.com>
2019-11-15 16:08:20 +0545

    Create pd-service-zato.py

---
+++ pd-service-zato.py
@@ -0,0 +1,34 @@
+# stdlib
+from datetime import datetime
+from json import dumps
+
+# Zato
+from zato.server.service import Service
+
+class SampleService(Service):
+
+  class SimpleIO:
+    input_required = ('id', 'subject', 'body', 'by', 'time')
+    output_required = ('id', 'title', 'content', 'author', 'sentAt')
+
+  def handle(self):
+
+    #xmlService = self.outgoing.plain_http.get('XML_Docs')
+    #jsonService = self.outgoing.plain_http.get('JSON_Docs')
+
+    request = self.request.input
+
+    self.logger.info('Request: {}'.format(request))
+
+    payload = {}
+    payload['id'] = request.id
+    payload['title'] = request.subject
+    payload['content'] = request.body
+    payload['author'] = request.by
+    payload['sentAt'] = request.time
+
+    params = {}
+    headers = {'Content-Type': 'application/json'}
+    response = jsonService.conn.post(self.cid, dumps(payload), params, headers=headers)
+
+    self.response.payload = payload
